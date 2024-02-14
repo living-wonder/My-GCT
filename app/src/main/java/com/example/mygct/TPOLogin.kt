3ae2ab2417c0128cdda7887d8050e0a40da0dddf
DIVAAGAR R B <divaagarrb@gmail.com>
2024-02-14 23:02:21 +0530

    Login Design finished

---
+++ app/src/main/java/com/example/mygct/TPOLogin.kt
@@ -0,0 +1,11 @@
+package com.example.mygct
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TPOLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tpologin)
+    }
+}

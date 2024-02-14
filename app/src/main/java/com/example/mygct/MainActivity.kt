6f5565546669e4f708dca1c29ae4fa23582b1395
DIVAAGAR R B <divaagarrb@gmail.com>
2024-02-14 23:01:52 +0530

    Login Design finished

--- app/src/main/java/com/example/mygct/MainActivity.kt
+++ app/src/main/java/com/example/mygct/MainActivity.kt
@@ -1,11 +1,64 @@
 package com.example.mygct
 
-import androidx.appcompat.app.AppCompatActivity
+
+
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var admin: ImageView
+    private lateinit var student: ImageView
+    private lateinit var tpo: ImageView
+    private lateinit var settings: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        admin = findViewById(R.id.admin)
+        student = findViewById(R.id.student)
+        tpo = findViewById(R.id.tpo)
+        settings = findViewById(R.id.settings)
+
+        admin.setOnClickListener{
+            adminClicked()
+        }
+        student.setOnClickListener{
+            studentClicked()
+        }
+        tpo.setOnClickListener{
+            tpoClicked()
+        }
+    }
+
+    fun adminClicked() {
+        val intent = Intent(this, AdminLogin::class.java)
+        startActivity(intent)
+    }
+
+    fun tpoClicked() {
+        val intent = Intent(this, TPOLogin::class.java)
+        startActivity(intent)
+    }
+
+    fun studentClicked() {
+        val intent = Intent(this, StudentLogin::class.java)
+        startActivity(intent)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
+        System.exit(0)
+    }
+
+    fun logoutTPO(view: View) {
+        // Handle logout TPO action if needed
     }
 }

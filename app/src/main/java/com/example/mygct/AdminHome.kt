7f4723a63ebd362fcb86b682877b57f014aa6f9f
DIVAAGAR R B <divaagarrb@gmail.com>
2024-02-14 23:20:44 +0530

    Admin

---
+++ app/src/main/java/com/example/mygct/AdminHome.kt
@@ -0,0 +1,50 @@
+package com.example.mygct
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class AdminHome : AppCompatActivity() {
+
+    private lateinit var addTpoBtn: Button
+    private lateinit var addStudentBtn: Button
+    private lateinit var logoutBtn :Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_home)
+
+        addTpoBtn = findViewById(R.id.addtpobtn)
+        addStudentBtn = findViewById(R.id.addstudentbtn)
+        logoutBtn=findViewById(R.id.logoutbtn)
+
+
+        addTpoBtn.setOnClickListener { addTpo() }
+        addStudentBtn.setOnClickListener { addStudent() }
+        logoutBtn.setOnClickListener { logoutAdmin() }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun addTpo() {
+       // val intent = Intent(this, AddingNewTPO::class.java)
+        startActivity(intent)
+    }
+
+    private fun addStudent() {
+      //  val intent = Intent(this, AddingNewStudent::class.java)
+        startActivity(intent)
+    }
+
+    fun logoutAdmin() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
